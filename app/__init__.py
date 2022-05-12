31c26a9e649ac82e9ad2cf7c2045764e14fc296b
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-12 17:43:20 +0300

    add the changes to the files and importations

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -10,12 +11,17 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+photos = UploadSet('photos',IMAGES)
+
 def create_app(config_name):
 
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://myles:m-pp2@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 class ProdConfig(Config):
