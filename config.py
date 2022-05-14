9e3bbdeb583e7899f08fe5060965543009b09d54
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-14 23:28:52 +0300

    initial commit

--- config.py
+++ config.py
@@ -4,6 +4,16 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://myles:m-pp2@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    WTF_CSRF_ENABLED = False
 
 
 class ProdConfig(Config):
